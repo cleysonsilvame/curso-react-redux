6cef58ba17778587be2b9490d9c497f19f461400
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-06-14 01:24:02 -0300

    colocando rotas e cors

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,11 +1,16 @@
 const port = 3003;
 
 const express = require("express");
+const cors = require("./cors");
 const server = express();
 
-server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors);
+
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}`)
 })
+
+module.exports = server
